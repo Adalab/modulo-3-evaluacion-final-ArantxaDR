a955abb89bf45dbd843d560b1171bb12ad5be54b
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-24 09:08:06 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../stylesheets/App.css";
+
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Hola mundo</h1>;
+  }
+}
+
+export default App;
