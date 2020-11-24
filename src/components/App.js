1da22beb71935abe24cc0baedd1dd78794fc651b
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-24 13:05:19 +0100

    add event listeners

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,30 @@ import CharacterList from "./CharacterList";
 import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import getDataFromApi from "../services/DataFromApi";
+import { computeHeadingLevel } from "@testing-library/react";
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const [nameFilter, setNameFilter] = useState("");
+  const [speciesFilter, setSpeciesFilter] = useState("All");
   useEffect(() => {
     getDataFromApi().then((data) => setItems(data));
   }, []);
+
+  const handleFilters = (data) => {
+    if (data.key === "name") {
+      setNameFilter(data.value);
+    } else if (data.key === "species") {
+      setSpeciesFilter(data.value);
+    }
+  };
+
   return (
     <>
       {/* <Header /> */}
       <h1 className="title">Buscador de personajes</h1>
       <main className="main">
-        <Filters />
+        <Filters handleFilters={handleFilters} />
         <CharacterList items={items} />
       </main>
       <Footer />
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,24 +1,46 @@
 import React from "react";
 
-const name = () => {
+const Filters = (props) => {
+  const handleChange = (ev) => {
+    ev.preventDefault();
+    props.handleFilters({ value: ev.target.value, key: ev.target.id });
+  };
+  const handleSubmit = (ev) => {
+    if (ev.key === "Enter") {
+      ev.preventDefault();
+    }
+  };
   return (
     <section>
-      <form>
+      <form className="form" onKeyDown={handleSubmit}>
         <label className="form_label" htmlFor="name">
           Filtrar por nombre
         </label>
-        <input className="form_input__text" type="name" name="name" id="name" />
+        <input
+          className="form_input__text"
+          type="name"
+          name="name"
+          id="name"
+          placeholder="¿Quién es tu favorito?"
+          onChange={handleChange}
+        />
 
         <label className="form_label" htmlFor="species">
           Especie
         </label>
-        <select className="form_input__text" name="species" id="species">
-          <option value="">Humano</option>
-          <option value="">Alien</option>
+        <select
+          className="form_input__text"
+          name="species"
+          id="species"
+          onChange={handleChange}
+        >
+          <option value="All">Todos</option>
+          <option value="Human">Humano</option>
+          <option value="Alien">Alien</option>
         </select>
       </form>
     </section>
   );
 };
 
-export default name;
+export default Filters;
