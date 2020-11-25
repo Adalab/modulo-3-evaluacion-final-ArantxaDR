613d094702b3c3c21e42901bc22feed4780a8b76
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-25 13:10:15 +0100

    fix bugs

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,19 +1,14 @@
 import React from "react";
 import errorImg from "../stylesheets/img/error-R&M.jpg";
-import { Link } from "react-router-dom";
-import gunPortal from "../stylesheets/img/gunportal.png";
 
 const ErrorPage = () => {
   return (
     <>
-      <Link to="/" className="home_link" title="Home l">
-        <img className="home_link__img" src={gunPortal} alt="Gunportal" />
-        <p className="home_link__text">Home </p>
-      </Link>
       <div className="container_character__notfound">
         <h2 className="title_character__notfound">
           Watch the show!!!!! That's not a character's name
         </h2>
+        <h3 className="title_character__tryagain">Try a new search</h3>
         <img className="img_notfound" src={errorImg} alt="Not found" />
       </div>
     </>
