e4fe5e9f556566fc1f3d2f55eec5a9c11174688f
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-25 11:06:25 +0100

    add footer css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header className="App-header">
-        <img src={headerImg} alt="Rick and Morty" />{" "}
+        <img src={headerImg} alt="Rick and Morty" />
         <h1 className="title">Take a look </h1>
       </header>
     );
